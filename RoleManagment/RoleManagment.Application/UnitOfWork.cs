19fd51215dc4be8d7604d17463c410588b086191
alireza <khazaeinezhadalireza@gmail.com>
2022-05-28 11:52:33 +0430

    Unit of work 20% compeleted

---
+++ RoleManagment/RoleManagment.Application/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿
+using RoleManagment.Core;
+
+namespace RoleManagment.Application
+{
+    public class UnitOfWork<T> where T : class
+    {
+        private RoleManagmentContext _context { get; }
+        private IRepository<T> _repository { get; }
+
+
+        public UnitOfWork(RoleManagmentContext context,
+            IRepository<T> repository)
+        {
+            _context = context;
+            _repository = repository;
+        }
+
+
+        private int Complete()
+        {
+            return _context.SaveChanges();
+        }
+        private void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
